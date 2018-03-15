98c29ff68bfc087eaa01c8296c318de0301e6a2e
Jonathan <sifreed77@gmail.com>
2018-03-14 21:46:08 -0400

    Added createschema implementation

---
+++ schema/CreateSchema.sql
@@ -0,0 +1,5 @@
+--Create a new schema by issuing the command CREATE SCHEMA followed by the name, for example students
+ CREATE SCHEMA students;
+
+--Create a new schema by issuing the command CREATE SCHEMA followed by the name, for example students
+ CREATE SCHEMA university;
