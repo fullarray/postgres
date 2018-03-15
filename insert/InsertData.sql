d8f63d590faa6cd34e2561412592063509a2844b
Jonathan <sifreed77@gmail.com>
2018-03-14 21:45:43 -0400

    Added insertdata implementation

---
+++ insert/InsertData.sql
@@ -0,0 +1,7 @@
+--To insert data use the INSERT INTO keywords. For example, to insert data into table students
+ INSERT INTO students (id, name, address, age)
+ VALUES (1, 'John Doe', '123 Main Street', 34);
+ 
+--To insert data use when one of the columns auto_increments. For example, the id columns auto_increments
+ INSERT INTO employees (name, address, age)
+ VALUES ('Maria Juarez', '464 Ocean Drive', 25);
