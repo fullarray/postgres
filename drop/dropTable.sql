e362f27aeeb345aaf83c7efeec2da31b0cf4c4dd
Jonathan <sifreed77@gmail.com>
2018-03-14 21:45:20 -0400

    Added droptable implementation

---
+++ drop/dropTable.sql
@@ -0,0 +1,11 @@
+--First list the tables in the current schema by issuing the the \d command
+ postgres # \d
+
+--use the following command to drop table. For example, to drop table named students.
+ DROP TABLE students;
+ 
+--use the following command to drop table. For example, to drop table named employees.
+ DROP TABLE employees;
+ 
+--to drop multiple tables. Separate each table with commas.
+ DROP TABLE employees, students;
