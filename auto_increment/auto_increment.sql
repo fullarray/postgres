2e110c8899101dc90012097f381be4216a4afee0
Jonathan <sifreed77@gmail.com>
2018-12-27 22:37:51 -0500

    Update auto_increment.sql

--- auto_increment/auto_increment.sql
+++ auto_increment/auto_increment.sql
@@ -5,3 +5,9 @@ create sequence employee_id_seq;
 alter table player alter employee_id set default nextval('employee_id_seq');
 Select setval('employee_id_seq', 1 );
 
+
+
+--declare a sequence, for instance student_id_seq 
+create sequence student_id_seq;
+alter table salary alter student_id set default nextval('student_id_seq');
+Select setval('student_id_seq', 1 );
